9d19654e96d0042e2ee46808cb3eabd25df8091c
Mykhaylo.Makarov <mykhaylo.makarov@adamsoftware.net>
2018-04-14 01:04:01 +0100

    Refactoring

--- Cryption/CryptorV1.cs
+++ Cryption/CryptorV1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 namespace SecuViewer.Cryption
 {
-    internal class CryptorV1:ICryptor
+    internal class CryptorV1 : ICryptor
     {
         private readonly byte[] _magic = {(byte) 'S', (byte) 'F', (byte) 'V', (byte) '1'};
 
@@ -12,7 +12,7 @@ namespace SecuViewer.Cryption
         {
             var offset = (reader.ReadByte() ^ data.Next()) & 0xFF;
             reader.Position = offset;
-            for (int i = 0; i < ((offset - 5) & 0xF); i++)
+            for (int i = 0; i < ((offset - (_magic.Length + 1)) & 0xF); i++)
             {
                 data.Next();
             }
@@ -24,7 +24,7 @@ namespace SecuViewer.Cryption
             writer.Write(_magic, 0, _magic.Length);
             var rnd = new Random();
             var garbageCount = rnd.Next(10, 250);
-            var totalPadding = (byte) (((garbageCount + 5) ^ data.Next()) & 0xFF);
+            var totalPadding = (byte) (((garbageCount + _magic.Length + 1) ^ data.Next()) & 0xFF);
             var buffer = new byte[garbageCount + 1];
             rnd.NextBytes(buffer);
             buffer[0] = totalPadding;
@@ -39,8 +39,8 @@ namespace SecuViewer.Cryption
         public bool CanDecrypt(Stream reader)
         {
             var buffer = new byte[4];
-            var read = reader.Read(buffer, 0, 4);
-            if (read == 4 && buffer.SequenceEqual(_magic)) return true;
+            var read = reader.Read(buffer, 0, _magic.Length);
+            if (read == _magic.Length && buffer.SequenceEqual(_magic)) return true;
             reader.Position = 0;
             return false;
         }
